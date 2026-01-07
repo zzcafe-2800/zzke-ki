5b5abf2ece66acf30dc049f9a6db8bc61d0f9fb4
zzcafe-2800 <yumewomirururu@gmail.com>
2026-01-07 10:18:03 +0900

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,8 @@
 
  // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-analytics.js";
+  import {  getFirestore, collection, doc, setDoc, onSnapshot, updateDoc, deleteDoc,
+  getDocs, serverTimestamp, getDoc, increment } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
 
